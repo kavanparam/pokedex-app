1567fc46bfa0026653bf30f67fa48917c9e887d6
kavanparam <kavan.param@gmail.com>
2022-07-24 21:44:09 -0400

    Updated scripts. Included search bar to fetch from API.
    Using JavaScript promises to fetch data with API — the pokemon's move list and sprite. Icon for the webpage and added overall styling. Updated HTML elements with scripts — default welcome page (before an initial search is made by user), hidden tag and webpage sections until a search is made, function run on button click.

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
+//For an initial Pokemon load -- depreciated
 function start(){
-    // Name
-    console.log("Hello World");
-    const pokemonName = 'Mew';
-    const pokemonNameMessage = "Pokemon Name: " + pokemonName;
-    document.getElementById("name").innerHTML = pokemonNameMessage; //changes HTML code
- 
-    // Image
-    const imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/151.png";
+    
+    const requestURL = "https://pokeapi.co/api/v2/pokemon/";
 
+    // //JavaScript promises
+    // fetch(requestURL+"snorlax").then((x) => x.json()).then((obj) => {
+    //     displayPokemon(obj);
+    // });
+}
+
+
+//Receives the JSON obj coming from PokeAPI
+function displayPokemon(obj){
+    // Name
+    const pokeName = obj.name;
+        
+    const pokemonNameMessage = "Pokemon Name: " + pokeName;
+    document.getElementById("name").innerHTML = pokemonNameMessage; //changes HTML code
+
+    // Image
+    const imageUrl = obj.sprites.front_default;
     document.getElementById('image').src = imageUrl;
+
+
+    // Move List
+    const move1 = obj.moves[0].move.name;
+    const move2 = obj.moves[1].move.name;
+    const move3 = obj.moves[2].move.name;
+    const move4 = obj.moves[3].move.name;
+    document.getElementById("move-list").innerHTML = `<li>${move1}</li> <li>${move2}</li> <li>${move3}</li> <li>${move4}</li>`;
+
+    //Additional Views
+    //Load the section with visibility none on search button click
+    document.getElementById("views-container").style.display = "block";
+}
+
+function fetchPokemon(){
+    const searchTerm = document.getElementById("search-bar").value;
+
+    const requestURL = "https://pokeapi.co/api/v2/pokemon/";
+
+    fetch(requestURL+searchTerm).then((x) => x.json()).then((obj) => {
+        displayPokemon(obj);
+    });
 }
