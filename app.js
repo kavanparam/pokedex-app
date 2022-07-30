711802eb854fe3898e44aad3610aaacd185b922d
kavanparam <kavan.param@gmail.com>
2022-07-30 04:13:15 -0400

    Some updates
    - Refactored code
    - Full-move-list cleared on new search
    - Updated favicon again with higher res

--- app.js
+++ app.js
@@ -20,12 +20,16 @@ function fetchPokemon(){
 
     fetch(requestURL+searchTerm).then((x) => x.json()).then((obj) => {
         displayPokemon(obj);
+        displayFullMoveList(obj);
     });
 }
 
 //Interpret the received object
 function displayPokemon(obj){
     document.getElementById("main-flex").style.display = "flex"; //Show main flex container
+    document.getElementById("full-move-list").style.display = "none"; //Hide full move list on new search
+    document.getElementById("views-container").style.display = "block";  //Show additional views section (on search button click)
+
 
     // Name
     document.getElementById("name").innerHTML = "Pokemon Name: " + obj.name; 
@@ -33,33 +37,23 @@ function displayPokemon(obj){
     // Image
     document.getElementById('image').src = obj.sprites.front_default;
 
-
     
     // Move list
     document.getElementById("move-list").innerHTML = "";
     let moveList = "";
 
-    //If there are 10 or more moves
+    //If there are 10 or more moves, display the first 10 moves
     if(Object.keys(obj.moves).length>=10){
-        //Display the first 10 moves
         for(let i=0; i<10; i++)
             moveList += `<li>${obj.moves[i].move.name}</li>`;
 
-        moveList += "<li><button id='show-full-list' onclick='fullMoveList()' style='opacity:0.9'> ... </button></li>"; //Prompt to show full move list
+        moveList += "<li><button id='show-full-list' onclick='document.getElementById(`full-move-list`).style.display = `block`' style='opacity:0.9'> ... </button></li>"; //Prompt to show full move list
 
-        let fullMoveList = "";
-        for(let i=10; i<Object.keys(obj.moves).length; i++){    //Hidden full-move-list that shows up on prompt click
-            fullMoveList += `<li>${obj.moves[i].move.name}, </li>`;
-        }
-        document.getElementById("full-move-list").innerHTML = fullMoveList;
-        
-
+        //JS method to adding clickable element
         // document.getElementById("show-full-list").addEventListener("click", fullMoveList);
 
-    }
-    //If there aren't 10 moves
-    else{
-        //Display all moves
+    //If there aren't 10 moves, display all moves
+    } else{
         for(i in obj.moves){
             moveList += `<li>${obj.moves[i].move.name}</li>`;
         }
@@ -67,9 +61,9 @@ function displayPokemon(obj){
     document.getElementById("move-list").innerHTML = moveList;
 
 
-    document.getElementById("views-container").style.display = "block";  //Show additional views section (on button click)
-
-    document.getElementById("image-flex").innerHTML = ""; //Resets images on new Pokemon search
+    
+    // Addditional views section
+    document.getElementById("image-flex").innerHTML = ""; //Clears images on new Pokemon search
     for(img in obj.sprites){
         if(typeof(obj.sprites[img]) === 'string'){
             // console.log(obj.sprites[img]);
@@ -80,9 +74,16 @@ function displayPokemon(obj){
 }
 
 //How to display full move list of the current pokemon object?
-function fullMoveList(){
+function displayFullMoveList(obj){
+    let fullMoveList = "";
+        for(let i=10; i<Object.keys(obj.moves).length; i++){    //Hidden full-move-list that shows up on prompt click
+            fullMoveList += `<li>${obj.moves[i].move.name}, </li>`;
+        }
+    document.getElementById("full-move-list").innerHTML = fullMoveList;
 
-    document.getElementById("full-move-list").style.display = "block";
+
+
+
     
     // console.log(obj.moves[0].move.name);
     
